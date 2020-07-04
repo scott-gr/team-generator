4bf6f8f099bfa74fb5c3bc220bf1433a4b698770
Scott Griffin <mscottgriffin@gmail.com>
2020-07-03 22:24:06 -0400

    added properties to manager. Fixed some mismatched properties in engineer and intern.

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,13 +1,11 @@
-// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
-// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
-const Intern = require("./Intern");
+const Employee = require("./Employee");
 class Intern extends Employee {
   constructor(name, id, email, school){
     super(name, id, email);
     this.school = school;
   }
   getSchool(){
-    return this.github;
+    return this.school;
   }
   //overrides employee role to return 'Engineer'
   getRole() {
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,16 @@
-// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+class Manager extends Employee {
+  constructor(name, id, email, officeNumber){
+    super(name, id, email);
+    this.officeNumber = officeNumber;
+  }
+  getOfficeNumber(){
+    return this.officeNumber;
+  }
+  //overrides employee role to return 'Engineer'
+  getRole() {
+    return this.constructor.name
+  }
+}
+
+module.exports = Manager;
