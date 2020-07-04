685de79ae54d7a5d8702f25e46415b9dfa19c0f4
Scott Griffin <mscottgriffin@gmail.com>
2020-07-03 21:54:56 -0400

    added properties to intern subclass

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -2,7 +2,17 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
 const Intern = require("./Intern");
 class Intern extends Employee {
-  //add constructor
+  constructor(name, id, email, school){
+    super(name, id, email);
+    this.school = school;
+  }
+  getSchool(){
+    return this.github;
+  }
+  //overrides employee role to return 'Engineer'
+  getRole() {
+    return this.constructor.name
+  }
 }
 
 module.exports = Intern;
