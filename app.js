bdf5ab9d29f2d1b431e39a129fadc618d73846c3
Scott Griffin <mscottgriffin@gmail.com>
2020-07-07 02:08:28 -0400

    beginning function to ask with inquirer, will switch between 3 roles

--- app.js
+++ app.js
@@ -1,38 +1,41 @@
 //Dependencies
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
 //output to html
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const OUTPUT_DIR = path.resolve(__dirname, 'output');
+const outputPath = path.join(OUTPUT_DIR, 'team.html');
 
-const render = require("./lib/htmlRenderer");
+const render = require('./lib/htmlRenderer');
 //empty array to be filled with generated employee objects
 const employees = [];
 
-//employee objects
-let manager;
-let engineer;
-let intern;
+const teamOutput = (path, data) => {
+  fs.writeFile(path, render(data), (error) => {
+    if (error) throw error;
+    else console.log('Output HTML success');
+  });
+};
 
+const askPrompt = () => {
+  inquirer.prompt(questions.employeeRoleQ).then((data) => {
+      switch(data.role) {
 
 //function to ask questions with inquirer
 
 //let employee
-  //if manager input, create new manager with data from fields
+//if manager input, create new manager with data from fields
 
-  //else if engineer input, create new manager with data from fields
+//else if engineer input, create new manager with data from fields
 
-  //else if intern input, create new manager with data from fields
+//else if intern input, create new manager with data from fields
 
-  //push
+//push
 
-  //write to file
-
-
+//write to file
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
@@ -62,6 +65,6 @@ let intern;
 /// types are subclasses
 /// 1 prompt
 /// 1 prompt what kind of team member
-/// another function inquirer.prompt 
+/// another function inquirer.prompt
 
 ///manager.test - calling get office # method, not mentioned in readme
