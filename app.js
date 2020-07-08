62a420caa47276f68193427d97183302b849cb98
Scott Griffin <mscottgriffin@gmail.com>
2020-07-07 21:08:26 -0400

    moved folders back in place, reinstalled npm and inquirer. Restored json

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 //Dependencies
-const Manager = require('./Develop/lib/Manager');
-const Engineer = require('./Develop/lib/Engineer');
-const Intern = require('./Develop/lib/Intern');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
 const path = require('path');
 const fs = require('fs');
-const questions = require('./Develop/lib/questions');
-const render = require('./Develop/lib/htmlRenderer');
+const questions = require('./lib/questions');
+const render = require('./lib/htmlRenderer');
 //output to html
 const OUTPUT_DIR = path.resolve(__dirname, 'output');
 const outputPath = path.join(OUTPUT_DIR, 'team-output.html');
